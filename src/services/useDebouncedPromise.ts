8e2ffcafe43f908f7366f985073c3ea714c3b391
AllanAbucarub <acoutinhoabucarub@gmail.com>
2020-12-01 22:59:55 -0300

    Added cencel previous request on search

--- src/services/useDebouncedPromise.ts
+++ src/services/useDebouncedPromise.ts
@@ -1,21 +1,31 @@
 import { useRef } from 'react'
-import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, CancelTokenSource } from 'axios'
 
 export default function useDebouncedPromise(fn: AxiosInstance, delay: number) {
   let timoutRef = useRef(0)
+  let cancelToken: CancelTokenSource;
 
   function handler<T>(params: AxiosRequestConfig): Promise<AxiosResponse<T>> {
     return new Promise<AxiosResponse<T>>((resolve, reject) => {
       if (timoutRef.current) {
+        if (typeof cancelToken != typeof undefined) {
+          cancelToken.cancel("Operation canceled due to new request.");
+        }
         clearTimeout(timoutRef.current!)
       }
+  
+      //Save the cancel token for the current request
+      cancelToken = axios.CancelToken.source();
 
       timoutRef.current = window.setTimeout(async () => {
         try {
-          const response = await fn.get<T>(params.url!, params)
+          const response = await fn.get<T>(params.url!, { ...params, cancelToken: cancelToken.token })
           resolve(response)
         } catch (e) {
-          reject(e)
+          console.log('teste');
+          if(!e.message.includes('Operation canceled')) {
+            reject(e)
+          }
         }
       }, delay)
     })
